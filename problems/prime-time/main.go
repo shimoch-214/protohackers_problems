1a2fd22e5ce058dd2bcc92373963c48d08f2ec8c
shimoch <mikadonoosoba2@gmail.com>
2023-01-28 23:36:13 +0900

    validate number is not nil

--- problems/prime-time/main.go
+++ problems/prime-time/main.go
@@ -25,12 +25,12 @@ func isPrimeNumber(number float64) bool {
 type PrimeTime struct{ *server.Config }
 
 type Request struct {
-	Method string  `json:"method"`
-	Number float64 `json:"number"`
+	Method string   `json:"method"`
+	Number *float64 `json:"number"`
 }
 
 func (req *Request) isValidRequest() bool {
-	return req.Method == "isPrime"
+	return req.Method == "isPrime" && req.Number != nil
 }
 
 type Response struct {
@@ -49,15 +49,15 @@ func (PrimeTime) Handle(conn net.Conn) {
 		var req Request
 		err := json.Unmarshal(reqMessage, &req)
 		log.Println(string(reqMessage))
+		log.Println(req)
 		if err != nil || !req.isValidRequest() {
 			log.Println(err)
 			resMessage = []byte("invalid request")
 			if _, err := conn.Write(append(resMessage, []byte("\n")...)); err != nil {
 				log.Fatal(err)
 			}
-			conn.Close()
 		} else {
-			isPrime := isPrimeNumber(req.Number)
+			isPrime := isPrimeNumber(*req.Number)
 			res := Response{
 				Method: "isPrime",
 				Prime:  isPrime,
